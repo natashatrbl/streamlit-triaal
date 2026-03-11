afdfd73119518f135255668ab2c270dc07caf950
natashatrbl <rebelanatasha@gmail.com>
2026-03-11 16:20:47 +0700

    Fourth Change

--- titanic_dashboard.py
+++ titanic_dashboard.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
+st.set_page_config(layout='wide')
 st.title("Titanic Dashboard")
+
+df = pd.read_csv(r"D:\PROJECT TASHA\PYTHON\learning\streamlit\data\titani_data.csv")
+
+df['Embarked'] = df['Embarked'].fillna('Unknown')
+
