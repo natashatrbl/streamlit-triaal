01a01400dd4da0f8c4950ca55e6c05df9de20543
natashatrbl <rebelanatasha@gmail.com>
2026-03-11 16:36:43 +0700

    Fifth Commit

--- titanic_dashboard.py
+++ titanic_dashboard.py
@@ -9,3 +9,15 @@ df = pd.read_csv(r"D:\PROJECT TASHA\PYTHON\learning\streamlit\data\titani_data.c
 
 df['Embarked'] = df['Embarked'].fillna('Unknown')
 
+embarked_port = list(df['Embarked'].unique())
+gender = list(df['Sex'].unique())
+
+col1, col2 = st.columns([1,1])
+selected_port = col1.selectbox(
+    options=embarked_port,
+    label='Select a port'
+)
+selected_gender = col2.selectbox(
+    options=gender,
+    label='Select gender'
+)
